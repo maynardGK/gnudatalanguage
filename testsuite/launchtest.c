b2c87b5218aee3d546f8dc50412990a547e84a50
Ole Streicher <olebole@debian.org>
2016-02-25 13:08:00 +0100

    Use the correct laiunchtest.c

--- testsuite/launchtest.c
+++ testsuite/launchtest.c
@@ -1,51 +1,62 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <malloc.h>
-#ifndef _MSC_VER
-#include <unistd.h>
-#endif
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include<sched.h>
 int main(int c,char**v) {
-char*p;
-if(c!=2) return 1;
-for(p=v[1];*p;++p);
-if(p-4<v[1]) return 1;
-if(p[-1]!='o'||p[-2]!='r'||p[-3]!='p'||p[-4]!='.') return 1;
-*(p-4)=0;
-
-_putenv("LC_COLLATE=C");
-_putenv("GDL_PATH=${BASE_SOURCE}/testsuite/;${BASE_SOURCE}/src/pro/");
-_putenv("GDL_STARTUP=");
-_putenv("IDL_STARTUP=");
-
-char *tmp = (char *)malloc( sizeof(char) * (strlen("${BASE_BINARY}/src/gdl -quiet -e ") + strlen(v[1]) + 1));
-//PROCESS_INFORMATION processInformation = {0};
-//STARTUPINFO startupInfo                = {0};
-//startupInfo.cb                         = sizeof(startupInfo);
-sprintf(tmp, "${BASE_BINARY}/src/gdl.exe -quiet -e %s", v[1]);
-//int rtn = CreateProcess(NULL, tmp, NULL, NULL, TRUE, 0, NULL, NULL, &startupInfo, &processInformation);
-int rtn = system(tmp);
-char cmd[512];
-snprintf(cmd, 511, "fgrep -q %s.pro ${BASE_SOURCE}/testsuite/tests.xfail",  v[1]);
-int xfail = (system(cmd) == 0);
-printf("%i: %s\n", xfail, cmd);
-
-if (rtn == 77) {
-    printf("TEST SKIPPED");
-    exit(0);
-} if (rtn == -1 || rtn == 127) {
-    printf("LAUNCH PROCESS FAILED");
+  char*p;
+  if(c!=2) return 1;
+  for(p=v[1];*p;++p);
+  if(p-4<v[1]) return 1;
+  if(p[-1]!='o'||p[-2]!='r'||p[-3]!='p'||p[-4]!='.') return 1;
+  *(p-4)=0;
+  
+  char cmd[512];
+  snprintf(cmd, 511, "fgrep -q %s.pro ${BASE_SOURCE}/testsuite/tests.xfail",
+	   v[1]);
+  int xfail = (system(cmd) == 0);
+  printf("%d: %s\n", xfail, cmd);
+  
+  setenv("LC_COLLATE","C",1);
+  setenv("GDL_PATH","+${BASE_SOURCE}/testsuite/:+${BASE_SOURCE}/src/pro/",1);
+  unsetenv("GDL_STARTUP");
+  unsetenv("IDL_STARTUP");
+  int devnull = open("/dev/null",O_RDONLY);
+  dup2(devnull, 0);
+  int child_pid = fork();
+  if(child_pid == 0) {
+    execl("${BASE_BINARY}/src/gdl","-quiet","-e",v[1],(char*)0);
     exit(1);
-} else if (rtn == 0) {
-    if (xfail) {
-      printf("TEST XFAIL but successful");
-    }
-    exit(0);
-} else {
-    if (xfail) {
-      printf("TEST XFAILED");
+  } else {
+    sched_yield();
+    int child_status;
+    waitpid(child_pid, &child_status, 0);
+    if (WEXITSTATUS(child_status) == 77) {
+      printf("TEST SKIPPED");
       exit(0);
+    } else if (WIFSIGNALED(child_status)) {
+      if (xfail) {
+	printf("XFAIL TEST EXITED FROM SIGNAL %d", WTERMSIG(child_status));
+	exit(0);
+      } else {
+	printf("TEST EXITED FROM SIGNAL %d", WTERMSIG(child_status));
+	exit(1);
+      }
+    } else if (WEXITSTATUS(child_status)) {
+      if (xfail) {
+	printf("XFAIL TEST");
+	exit(0);
+      } else {
+	exit(WEXITSTATUS(child_status));
+      }
     } else {
-      exit(rtn);
+      if (xfail) {
+	printf("XFAIL TEST SUCCESS");
+      }
+      exit(0);
     }
-}
+  }
 }
